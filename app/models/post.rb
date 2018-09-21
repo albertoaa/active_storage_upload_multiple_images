adb87d02cb0619e72c78f1a29628858b5e3755d4
Alberto Aragon <alberto@alturasoluciones.com>
2018-09-21 16:04:35 -0500

    Adding validations for presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post < ApplicationRecord
   has_many_attached :images
+  validates :title, presence: true
+  validates :body, presence: true
+  validate :image_type
 
   def thumbnail input
     return self.images[input].variant(resize: '300x300').processed
+  end
+
+  private
+  def image_type
+    if images.attached? == false
+      errors.add(:images, "are missing")
+    end
   end
 end
