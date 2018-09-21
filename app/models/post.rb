9a7132217bc652e5013486793c69a4ae790cace8
Alberto Aragon <alberto@alturasoluciones.com>
2018-09-21 12:31:11 -0500

    adjusting images size taking into account aspect ratio

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   has_many_attached :images
 
   def thumbnail input
-    return self.images[input].variant(resize: '300x300!').processed
+    return self.images[input].variant(resize: '300x300').processed
   end
 end
